1c8c7870c264b935ec46251c337f9e7aaa68d935
Bastian0089 <bastian00890@gmail.com>
2024-05-03 15:00:50 -0400

    Tutorial Completo
    Se completo el tutorial

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import {router} from "@/router/router.js";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
